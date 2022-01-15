84c8e08afc881cd7d8ce1f2730a595f48a49ca73
KonlawatPach <pach.wongsrithong@Gmail.com>
2022-01-15 18:08:10 +0800

    first-add

---
+++ hostroom.js
@@ -0,0 +1,33 @@
+//setInterval(function(){ location.reload(); }, 1000);
+function rollgacha() {
+    document.getElementById("gachabox").style.backgroundImage = "url('http://myweb.cmu.ac.th/konlawat_wong/picture/wanwai_burapa_speed.gif	')";
+
+    for (let i = 0; i < 1600; i+=200) {
+
+    setTimeout(function()
+    {
+        document.body.style.backgroundColor = "lightsteelblue";
+    }, i)
+
+    i+=200;
+    setTimeout(function()
+    {
+        document.body.style.backgroundColor = "royalblue";
+    }, i)
+
+    }
+    setTimeout(function()
+    {
+        document.body.style.backgroundColor = "slateblue";
+        const item = ["เกลือหมายเลข 1", "เกลือหมายเลข 2", "เกลือหมายเลข 3", "เกลือหมายเลข 4", "เกลือหมายเลข 5", "rickroll", "rickroll", "นายก็เกเรเหมือนกันนะเนี่ย"];
+        let num = Math.floor((Math.random()*10)%8);
+        alert("คุณได้รับ"+item[num]);
+        document.getElementById("gachabox").style.backgroundImage = "url('http://myweb.cmu.ac.th/konlawat_wong/picture/wanwai_burapa.gif')";
+        if(num == 5 || num == 6){window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");}
+        if(num == 7){window.open("https://www.youtube.com/watch?v=xmrzbfkIfaM");}
+    }, 2000)
+    
+}
+
+// https://jsonplaceholder.typicode.com/posts
+//https://datatables.net/examples/data_sources/js_array
