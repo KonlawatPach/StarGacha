02259e21d8a486eb8c20804336e53e359d94605d
KonlawatPach <pach.wongsrithong@Gmail.com>
2022-01-28 15:11:25 +0800

    delete img path

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-var storageRef = firebase.storage().ref();
+function send(){
+    var ref = firebase.storage().ref();
+    var file = document.querySelector("#inputGroupFile01").files[0]
+    var name = file.name
+    const metadata = {
+        contentType:file.type
+    }
+    const task = ref.child(name).put(file, metadata)
+}
